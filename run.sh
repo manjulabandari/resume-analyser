b7bd3d9dda8b974bfa3a72db4c1dab32855ee035
manjulabandari <manjulabandari21@gmail.com>
2026-03-26 10:18:26 +0530

    Add run.sh script to set up and run application

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create virtual environment
+python -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate  # On Windows: venv\Scripts\activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the application
+python app.py
